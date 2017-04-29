0a4457d49ddf805eb99a32af036e7f27e047df0c
Carlo Santos <carlox7@gmail.com>
2017-04-29 14:31:29 -0700

    refactored for resubmit

---
+++ lab-carlo/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const readText = require('./lib/lab3.js');
+
+readText(function(textFile) {
+  console.log(textFile);
+});
--- lab-carlo/lib/lab3.js
+++ lab-carlo/lib/lab3.js
@@ -1,1 +1,24 @@
+'use strict';
 
+const fs = require('fs');
+
+module.exports = function(callback) {
+  let hexText = [];
+
+  fs.readFile(`${__dirname}/../data/textOne.txt`, function(err, data){
+    if(err) throw err;
+    hexText.push(data.toString('hex', 0, 8));
+
+    fs.readFile(`${__dirname}/../data/textTwo.txt`, function(err, data){
+      if(err) throw err;
+      hexText.push(data.toString('hex', 0, 8));
+
+      fs.readFile(`${__dirname}/../data/textThree.txt`, function(err, data){
+        if(err) throw err;
+        hexText.push(data.toString('hex', 0, 8));
+        console.log(hexText);
+        if(callback) return(callback(hexText));
+      });
+    });
+  });
+};
